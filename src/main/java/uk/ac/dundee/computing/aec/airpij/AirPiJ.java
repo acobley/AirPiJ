4ba1c0acbf0d6c36cd2f0a8cec9ceba90b40215c
Andy C <acobley@computing.dundee.ac.uk>
2015-02-22 12:53:38 +0000

    Main Class

---
+++ src/main/java/uk/ac/dundee/computing/aec/airpij/AirPiJ.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.dundee.computing.aec.airpij;
+
+/**
+ *
+ * @author Administrator
+ */
+public class AirPiJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
