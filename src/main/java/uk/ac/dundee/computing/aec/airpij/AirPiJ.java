79889ce664433e85cfc3633c6c1d48babdb29764
Andy Cobley <acobley@computing.dundee.ac.uk>
2015-02-23 09:38:32 +0000

    temp main class using https://github.com/Pi4J/pi4j/blob/develop/pi4j-i2c-device/src/test/java/TestADXL345.java

--- src/main/java/uk/ac/dundee/computing/aec/airpij/AirPiJ.java
+++ src/main/java/uk/ac/dundee/computing/aec/airpij/AirPiJ.java
@@ -5,6 +5,10 @@
  */
 package uk.ac.dundee.computing.aec.airpij;
 
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+import com.pi4j.io.i2c.I2CFactory;
+
 /**
  *
  * @author Administrator
@@ -16,6 +20,48 @@ public class AirPiJ {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            I2CBus bus = I2CFactory.getInstance(1);
+            I2CDevice adxlDevice = bus.getDevice(0x53);
+
+            adxlDevice.write(0x31, (byte) 0x0b); // Initialize
+            long now = System.currentTimeMillis();
+            int measurement = 0;
+            adxlDevice.write(0x2D, (byte) 0x08); // Triger measuring
+            while (System.currentTimeMillis() - now < 10000) {
+                byte[] data = new byte[6];
+                adxlDevice.read(0x32, data, 0, 6);
+                int x = ((data[0] & 0x1f) << 8) + (data[1] & 0xff);
+                if ((data[0] & 0x80) == 1) {
+                    x = -x;
+                }
+                int y = ((data[0] & 0x1f) << 8) + (data[1] & 0xff);
+                if ((data[0] & 0x80) == 1) {
+                    y = -y;
+                }
+                int z = ((data[0] & 0x1f) << 8) + (data[1] & 0xff);
+                if ((data[0] & 0x80) == 1) {
+                    z = -z;
+                }
+                String sm = toString(measurement, 3);
+                String sx = toString(x, 7);
+                String sy = toString(y, 7);
+                String sz = toString(y, 7);
+                System.out.print(sm + sx + sy + sz);
+                for (int i = 0; i < 24; i++) {
+                    System.out.print((char) 8);
+                }
+                Thread.sleep(100);
+                measurement++;
+            }
+        } catch (Exception et) {
+            System.out.println("Can't get bus " + et);
+        }
     }
-    
+
+    public static String toString(int i, int l) {
+        String s = Integer.toString(i);
+        return " ".substring(0, l - s.length()) + s;
+    }
+
 }
